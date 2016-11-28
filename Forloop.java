6d85950f248ccd075f174ffe0b76d6bb7241c482
chakkamounika <chakkamounika30@gmail.com>
2016-11-28 21:37:52 +0530

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,17 @@
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int a=0;a<=5;a++)
+		{
+			for(int b=5;b>0;b--)
+			{
+				System.out.print(b );
+			}
+			System.out.println("a is" +a );
+			}
+			
+	}
+
+}
---
+++ Whileloop.java
@@ -0,0 +1,13 @@
+
+public class Whileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		while(a>0){
+			System.out.println("a is:" +a);
+a--;
+		}
+	}
+
+}
