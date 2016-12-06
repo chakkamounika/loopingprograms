77397b1fbf988b7de7fd09cbf3e9171cd226a179
chakkamounika <chakkamounika30@gmail.com>
2016-12-06 15:54:29 +0530

    Add files via upload

---
+++ Examplearray.java
@@ -0,0 +1,18 @@
+package examplearray;
+
+public class Examplearray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]=new int[5];
+		a[0]=3;
+		a[1]=7;
+	    a[2]=9;
+	    a[3]=12;
+	    a[4]=15;
+	    for(int i=0;i<=4;i++)
+	    	System.out.println(a[i]);
+
+	}
+
+}
