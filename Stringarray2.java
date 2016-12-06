d7498c485de420909382ed3eae3ba72449431243
chakkamounika <chakkamounika30@gmail.com>
2016-12-06 17:00:57 +0530

    Add files via upload

---
+++ Stringarray2.java
@@ -0,0 +1,20 @@
+package stringarray2;
+
+public class Stringarray2{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String  m[]=new String[8];
+		m[0]="I";
+		m[1]="am";
+		m[2]="not";
+		m[3]="wrong";
+		m[4]="i";
+		m[5]="just";
+		m[6]="do";
+		m[7]="mistakes";
+		for(int i=0;i<=7;i++)
+			System.out.println(m[i]);
+	}
+
+}
